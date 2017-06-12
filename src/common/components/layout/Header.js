6ff7fc47b9dd9674a556d2545a1f30a3f1010d87
James Waller <james.waller@independent.co.uk>
2017-06-12 17:37:02 +0100

    end of day commit.

--- src/common/components/layout/Header.js
+++ src/common/components/layout/Header.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 
 class Header extends Component {
 
-  render() {
-    return (
-    <div className="masthead">
-			<div className="container">
-			  <h3 className="masthead-title">
-			    <a href="/" title="Home">James Waller</a>
-			    <small>Front End Dev/UX Designer based in London</small>
-			  </h3>
-			</div>
-		</div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "masthead" >
+            <
+            div className = "container" >
+            <
+            h3 className = "masthead-title" >
+            <
+            a href = "/"
+            title = "Home" > James Waller < /a> <
+            small > Front End Dev / UX Designer < /small> <
+            /h3> <
+            /div> <
+            /div>
+        );
+    }
 }
 
 export default Header;
