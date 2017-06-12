bd0b78ee43159732c7ff1588bc8da25788f2538b
waldinho <james.waller1984@gmail.com>
2017-06-12 16:43:30 +0100

    Add files via upload

---
+++ src/common/components/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Banner from './layout/Banner';
+
+class Home extends Component {
+
+  constructor(props){
+    super(props);
+    this.eventToggleSidebar = this.eventToggleSidebar.bind(this)
+  }
+
+  eventToggleSidebar(e) {
+    e.preventDefault();
+    this.props.toggleSidebar(!this.props.layout.sidebarOpen);
+  }
+
+  render() {
+
+    return (
+
+      	<div className="posts">
+
+  		</div>
+  
+    );
+  }
+}
+
+export default Home;
