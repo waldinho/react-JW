f351404c9e32780c1d9bbcbf626fead5b6a9828b
James Waller <james.waller@independent.co.uk>
2017-06-13 17:34:00 +0100

    front page greeting added

--- src/common/components/Home.js
+++ src/common/components/Home.js
@@ -21,7 +21,12 @@ class Home extends Component {
 
       	<div className="posts">
           <div className="picture-bg">
-          <h1><span>
+
+<div className="circle"></div>
+  <h1><span>Hello</span><br/><span>and</span><br/><span>Welcome!</span><br/></h1>
+
+
+          {/*<h1><span>
           Blah blah blah
           </span></h1>
 
@@ -31,7 +36,7 @@ class Home extends Component {
 
           <h1><span>
           Blah blah blah
-          </span></h1>
+          </span></h1>*/}
 
             </div>
         </div>
