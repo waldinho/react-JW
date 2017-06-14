cf69c16f9f0d534ea535fb5a2c89b2d2c7b54bc9
James Waller <james.waller@independent.co.uk>
2017-06-14 13:19:35 +0100

    logo effect added to front page.

--- src/common/components/Home.js
+++ src/common/components/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Banner from './layout/Banner';
 
 class Home extends Component {
 
@@ -22,8 +21,8 @@ class Home extends Component {
       	<div className="posts">
           <div className="picture-bg">
 
-<div className="circle"></div>
-  <h1><span>Hello</span><br/><span>and</span><br/><span>Welcome!</span><br/></h1>
+          <div className="picture-bg-holder"><div className="circle"></div></div>
+            <h1><span>Hello</span><br/><span>and</span><br/><span>Welcome!</span><br/></h1>
 
           {/*<h1><span>
           Blah blah blah
@@ -37,7 +36,7 @@ class Home extends Component {
           Blah blah blah
           </span></h1>*/}
 
-            </div>
+          </div>
         </div>
 
   		
--- src/common/components/Services.js
+++ src/common/components/Services.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import Banner from './layout/Banner';
 import classNames from 'classnames';
 
 
@@ -7,11 +6,11 @@ class Services extends Component {
 
   render () {
     return (
-      <div className="white-bg">
+      <div>
 
       	  <div className="posts">
 						<div className="white-bg"> 
-                <div className="title"><h2>About</h2></div>
+                <div className="title"><h2>Services</h2></div>
                 <div className="main-content"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
              </div>
 	      </div>
