ca89d7970e108494191c77c1c6cccc62dadc61c1
James Waller <james.waller@independent.co.uk>
2017-11-06 09:16:27 +0000

    all pages added for more/case study links.

--- src/common/routes.js
+++ src/common/routes.js
@@ -11,7 +11,16 @@ import HomePage from "./containers/HomePage";
 //Redux Dumb
 import PortfolioPage from "./components/Portfolio";
 import ServicesPage from "./components/Services";
+import IndependentPage from "./components/independent";
+import StandardPage from "./components/standard";
+import EbayPage from "./components/ebay";
+import StubHubPage from "./components/stub-hub";
+import KurtGeigerPage from "./components/kurt-geiger";
+import ShellPage from "./components/shell";
+import TradefairPage from "./components/tradefair";
+import BetVictorPage from "./components/betvictor";
 import LCGPage from "./components/LCG";
+import TheSmallsPage from "./components/the-smalls";
 import error404 from "./components/404";
 
 export default (
@@ -19,7 +28,16 @@ export default (
       <Route path="home" component={HomePage} />
       <Route path="portfolio">
           <IndexRoute component={PortfolioPage}/>
+          <Route path="independent" component={IndependentPage} />
+          <Route path="evening-standard" component={StandardPage} />
+          <Route path="ebay" component={EbayPage} />
+          <Route path="stub-hub" component={StubHubPage} />
+          <Route path="kurt-geiger" component={KurtGeigerPage} />
+          <Route path="shell" component={ShellPage} />
+          <Route path="tradefair" component={TradefairPage} />
+          <Route path="betvictor" component={BetVictorPage} />
           <Route path="lcg" component={LCGPage} />
+          <Route path="the-smalls" component={TheSmallsPage} />
       </Route>      
       <Route path="services" component={ServicesPage} />
       <Route path="about" component={AboutPage} />
