0bce17c5439732be4d3fbe77e1a9d37b3d951411
waldinho <james.waller1984@gmail.com>
2017-11-07 10:03:06 +0000

    Update the-smalls.js

--- src/common/components/the-smalls.js
+++ src/common/components/the-smalls.js
@@ -13,7 +13,7 @@ class Services extends Component {
                 {/*<div className="title-circle"></div>*/}
                 <div className="title-services"><h2>The Smalls</h2></div>
                 <div className="main-content pad-50">
-                  <p>My core responsibility was to look after The Smalls website. In this varied role I was exposed to all types of development work as I was the sole internal developer at the company. I primary worked on the front end of these websites but have also experienced working on server side technologies, from database administration using MySQL to full on server migration. I looked after a number of CMS driven websites (Drupal and Wordpress) and I was given a large say in the look and feel of the websites we produced as well as writing documentation and bug fixing. This role was predominantly web based but I also developed applications and tools for internal use as well.</p>
+                  <p>In this varied role I was exposed to all types of development work as I was the sole internal developer at the company. I primary worked on the front end of these websites but have also experienced working on server side technologies, from database administration using MySQL to full on server migration. I looked after a number of CMS driven websites (Drupal and Wordpress) and I was given a large say in the look and feel of the websites we produced as well as writing documentation and bug fixing. This role was predominantly web based but I also developed applications and tools for internal use as well.</p>
                 </div>
               </div>
           </div>
