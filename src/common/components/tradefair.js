382b79aa5b2f567977100622f27f4c93e39406a7
James Waller <james.waller@independent.co.uk>
2017-11-17 11:06:43 +0000

    trade fair text added.

--- src/common/components/tradefair.js
+++ src/common/components/tradefair.js
@@ -14,7 +14,8 @@ class Tradefair extends Component {
               <div className="company-image pt-img-7"></div>
               <div className="center"><Link to="/portfolio" className="more"><span className="larger-arrow">&gt;</span>Back</Link></div>
                 <div className="main-content pad-50">
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  <p>During my time at London Capital Group, I was assigned the task of re-working Tradefair's (a client of London Capital Groups) online application form for their live and demo trading accounts. This task was Front End focused, it involved me taking a pre-existing design produced by a Graphic designer and turning that into complicit clean and tidy code. The application form interacted with the live and demo accounts databases so I had to be very aware of this while making any changes to the mark up and styling.</p>
+                  <p>The project turned out to be a big success, was delivered to the clients satisfaction and within the delivery deadline. Although the branding and styling has changed since I completed this project in 2010, you can see <Link to="https://tradefair.applyforanaccount.com/EN-GB/TF/live/step1.aspx" target="_blank">here</Link> that the application form I worked on is still used today.</p>
                 </div>
               </div>
           </div>
