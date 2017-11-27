e6ae33fcf12f3d0e59864c73bc00518e027fbf1a
James Waller <james.waller@independent.co.uk>
2017-11-27 11:08:02 +0000

    copy update.

--- src/common/components/the-smalls.js
+++ src/common/components/the-smalls.js
@@ -14,7 +14,10 @@ class Smalls extends Component {
               <div className="company-image pt-img-10"></div>
               <div className="center"><Link to="/portfolio" className="more"><span className="larger-arrow">&gt;</span>Back</Link></div>
                 <div className="main-content pad-50">
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  <p>My core responsibility was to look after the day to day running of The Smalls Website. In this varied role I was exposed to all types of development work as I was the sole internal developer at the company.</p> 
+                  <p>I primary worked on the front end of the website but also experienced working on server side technologies, from database administration using MySQL to full on server migration whilst working at The Smalls.</p>
+                  <p>The Smalls website was CMS driven (Drupal) and I was given a large say in the look and feel of the website we produced, particularaly when The Smalls went through a re-design process, which I was heavily involved in, with the implimentation of the new front end and UX of the website.</p> 
+                  <p>I was als responsible for writing documentation and bug fixing. This role was predominantly web based but I developed applications and tools for internal use as well.</p>
                 </div>
               </div>
            </div>
