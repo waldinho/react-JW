3524e0a49abf081d4c9d8fa07b5890bf4e1f2e8f
James Waller <james.waller1984@gmail.com>
2018-01-04 15:49:33 +0000

    copy and new ES img added.

--- src/common/components/standard-after.js
+++ src/common/components/standard-after.js
@@ -14,11 +14,15 @@ class After extends Component {
               <div className="company-image pt-img-2"></div>
               <div className="center"><Link to="/portfolio" className="more"><span className="larger-arrow">&gt;</span>Back</Link></div>
                 <div className="main-content pad-50">
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  <p>During summer 2017, I undertook a huge prjoect for The Evening Standard, the task was to re-work the whole of the site's front end. As you may or may not know The Evening Standard is a huge website, so this was no mean feat.</p>
+                  <p>I recieved the designs from the internal Graphic Designer. The designs were very different from the old site, as well as this we had decided to make the site more modular so sections of the site were now controlled in modules on the front and back end. So this meant changing the SASS and the template structuring.</p>
+                  <p>The templates were built using ejs templates, this allowed us to parse the data in using a json feed. The whole site worked on the MVC framework structuring principle and as you can see from the before and after shots below I think it turned out very well.
+                  </p>
                   <div className="tabs">
                     <Link to="/portfolio/evening-standard" className="tab">Before</Link>
                     <Link to="/portfolio/evening-standard-after" className="tab active-tab">After</Link>
                   </div>
+                  <div className="es-new"></div>
                 </div>
               </div>
           </div>
