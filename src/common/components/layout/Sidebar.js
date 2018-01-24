40892fe1adaa7d57d7c0ad44855d1e6b1cdcfd3f
James Waller <james.waller1984@gmail.com>
2018-01-25 09:46:23 +1100

    new URL for CV

--- src/common/components/layout/Sidebar.js
+++ src/common/components/layout/Sidebar.js
@@ -32,7 +32,7 @@ class Sidebar extends Component {
 		    <Link to="/portfolio" className="sidebar-nav-item"  activeClassName="active">Portfolio</Link>
 		    <Link to="/services" className="sidebar-nav-item" activeClassName="active">Services</Link>
 		    <Link to="/about" className="sidebar-nav-item" activeClassName="active">About</Link>
-				<Link to="http://www.james-waller.co.uk/CV" className="sidebar-nav-item" activeClassName="active" target="_blank">CV</Link>
+				<Link to="http://cv.james-waller-digital.com" className="sidebar-nav-item" activeClassName="active" target="_blank">CV</Link>
 		  </nav>
 
 			<div className="icon-wrap">
